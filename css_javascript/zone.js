9ce11a23a812c4893e69d221501019921939a86c
leocolo98 <l.colombini@studenti.unipi.it>
2022-05-17 11:21:57 +0200

    Modificato file zone.js

--- css_javascript/zone.js
+++ css_javascript/zone.js
@@ -1,29 +1,22 @@
 function gestoreEvidenzia(name){
-    try{
-        var x= name.substr(0,15);
-        if (x == "iniziomessaggio"){
-            var y= name.replace("iniziomessaggio", "salute");
-            var z= document.getElementById(y);
-        } else {
-            var z= document.getElementById(name);
-        }
-        z.style.color="red";
-    } catch (e) {
-        alert( 'gestoreEvidenzia' + e);
+    var x= name.substr(0,15);
+    if (x == "iniziomessaggio"){
+        var y= name.replace("iniziomessaggio", "salute");
+        var z= document.getElementById(y);
+    } else {
+        var z= document.getElementById(name);
     }
+    z.style.color="red";
 }
 
 function gestoreDisEvidenzia(name){  
-    try {
-        var x= name.substr(0,15);
-        if (x == "iniziomessaggio"){
-            var y= name.replace("iniziomessaggio", "salute");
-            var z= document.getElementById(y);
-        } else {
-            var z= document.getElementById(name);
-        }
-        z.style.color="#faebd7";
-    } catch (e) {
-        alert( 'gestoreDisevidenzia' + e);
-    }                                  
+    var x= name.substr(0,15);
+    if (x == "iniziomessaggio"){
+        var y= name.replace("iniziomessaggio", "salute");
+        var z= document.getElementById(y);
+    } else {
+        var z= document.getElementById(name);
+    }
+    z.style.color="#faebd7";
+                                    
 }
